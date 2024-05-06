f5d1d584d0aa6854c398be2c2f15a6b933d2d59b
phuonganh <phuonganh241976@gmail.com>
2024-05-06 15:06:49 +0700

    hi

---
+++ src/baiTap9.java
@@ -0,0 +1,9 @@
+public class baiTap9 {
+    public static void main(String[] args) {
+        int i = 4;
+        while (i <= 20) {
+            System.out.print(i + " ");
+            i = i + 4;
+        }
+    }
+}
